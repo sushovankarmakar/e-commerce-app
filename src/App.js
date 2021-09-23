d6e85a75351369ebe225b6dca0a883f905a38b90
Sushovan <officialkarmakar@gmail.com>
2021-09-23 08:06:26 +0530

    Login and Register Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import ProductDetails from "./pages/ProductDetails";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 function App() {
-  return <ProductDetails />;
+  return <Login />;
 }
 
 export default App;
